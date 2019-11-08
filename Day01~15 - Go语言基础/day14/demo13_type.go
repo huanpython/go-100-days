40e8feb21d6525d82e29503d8194134adf5dc566
huanfuan <huanfuan@163.com>
2019-11-08 17:12:46 +0800

    day14 方法、接口、类型

--- Day01~15 - Go语言基础/day14/demo13_type.go
+++ Day01~15 - Go语言基础/day14/demo13_type.go
@@ -6,3 +6,38 @@
 */
 
 package main
+
+import "fmt"
+
+type Person struct {
+	name string
+}
+
+func (p Person) show() {
+	fmt.Println("Person--->", p.name)
+}
+
+//类型别名
+type People = Person
+
+func (p People) show2() {
+	fmt.Println("People--->", p.name)
+}
+
+type Student struct {
+	People
+	Person
+}
+
+func main() {
+	var s Student
+	s.Person.name = "王二狗"
+	s.Person.show()
+
+	fmt.Printf("%T,%T\n", s.Person, s.People)
+
+	s.People.name = "李小花"
+	s.People.show()
+
+	s.People.show()
+}
