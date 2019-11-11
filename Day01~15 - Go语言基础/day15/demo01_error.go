903ff12a532a5705f480d4a7be7f673d2a65c0aa
huanfuan <huanfuan@163.com>
2019-11-12 00:13:21 +0800

    error

--- Day01~15 - Go语言基础/day15/demo01_error.go
+++ Day01~15 - Go语言基础/day15/demo01_error.go
@@ -6,3 +6,22 @@
 */
 
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("text.txt")
+	if err != nil {
+		fmt.Println(err)
+		if ins, ok := err.(*os.PathError); ok {
+			fmt.Println("1.Op:", ins.Op)
+			fmt.Println("2.Path:", ins.Path)
+			fmt.Println("3.Err:", ins.Err)
+		}
+		return
+	}
+	fmt.Println(f.Name(), "打开文件成功。。")
+}
--- Day01~15 - Go语言基础/day15/demo04_error.go
+++ Day01~15 - Go语言基础/day15/demo04_error.go
@@ -6,3 +6,17 @@
 */
 
 package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func main() {
+	files, _ := filepath.Glob("[")
+	//if err != nil && err == filepath.ErrBadPattern{
+	//	fmt.Println(err) //syntax error in pattern
+	//	return
+	//}
+	fmt.Println("files:", files)
+}
