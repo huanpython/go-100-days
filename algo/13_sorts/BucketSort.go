ec10ec931bece1e094208f812cabd9258d90506e
huanfuan <huanfuan@163.com>
2019-12-02 17:12:33 +0800

    sort

---
+++ algo/13_sorts/BucketSort.go
@@ -0,0 +1,7 @@
+/*
+@Time : 2019/12/2 0002 下午 4:33
+@Author : huanfuan
+@File : BucketSort
+@Software: GoLand
+*/
+package _3_sorts
---
+++ algo/14_sorts/CountingSort.go
@@ -0,0 +1,37 @@
+/*
+@Time : 2019/12/2 0002 下午 5:06
+@Author : huanfuan
+@File : CountingSort
+@Software: GoLand
+*/
+package _4_sorts
+
+import "math"
+
+func CountingSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
+
+	var max int = math.MaxInt32
+	for i := range a {
+		if a[i] > max {
+			max = a[i]
+		}
+	}
+
+	c := make([]int, max+1)
+	for i := range a {
+		c[a[i]]++
+	}
+	for i := 1; i <= max; i++ {
+		c[i] += c[i-1]
+	}
+	r := make([]int, n)
+	for i := n - 1; i >= 0; i-- {
+		index := c[a[i]] - 1
+		r[index] = a[i]
+		c[a[i]]--
+	}
+	copy(a, r)
+}
